40cbbb2a576e45924ceee8cf1c551de673e1b168
Pooja Sharma <pooja.sharma@rxpservices.com>
2017-03-27 16:02:52 +1100

    initial webpack and project setup

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from '../src/components/header/Header';
+
+ReactDOM.render(<Header/>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+	entry: './src/index.js',
+	output:{
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module:{
+		loaders: [
+			{
+				test:/\.jsx?$/,
+				include: path.join(__dirname,'src'),
+				loader: "babel-loader",
+        query:
+        {
+           presets: ['es2015', 'react']
+        }		
+			},
+      {
+        test: /\.css$/,
+        include: path.join(__dirname,'src'),
+        loader: "style-loader!css-loader"
+      },
+      {
+        test: /\.(png|jpg|ttf|eot)$/, 
+        include: path.join(__dirname,'src'),
+        loader: 'url-loader?limit=10000'
+      }
+		]
+	},
+  resolve: {
+    extensions: ['.js', '.es6']
+  }
+};
