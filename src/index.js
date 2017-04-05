ae5a95ac572e6b615b7e50b472f0fde03c04020e
Pooja Sharma <pooja.sharma@rxpservices.com>
2017-04-05 14:42:03 +1000

    routes in its own file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router,Route,IndexRoute,hashHistory} from 'react-router';
+import { Router,hashHistory} from 'react-router';
+import Routes from '../src/routes.js'
 import App from '../src/App.js'
-import Home from '../src/components/home/Home';
-import Login from '../src/components/login/Login';
-import Products from '../src/components/products/Products';
-import userApi from '../src/Api/userApi.js';
 
-ReactDOM.render(<Router history={hashHistory}>
-         <Route path="/" component={App}>
-         	 <IndexRoute component={Home}/>
-	         <Route path="/login" component={Login}/>
-	         <Route path="/products" component={Products}/>
-	         <Route path="/products/:productName" component={Products}/>
-         </Route>
-    </Router>, document.getElementById('app'));
+ReactDOM.render(<Router history={hashHistory} routes={Routes} component={App}/>, document.getElementById('app'));
--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link, IndexRoute, hashHistory, browserHistory,RouteHandler } from 'react-router';
+import { Router,Route,IndexRoute,hashHistory} from 'react-router';
+import App from '../src/App.js'
 import Home from '../src/components/home/Home';
 import Login from '../src/components/login/Login';
-import App from '../src/App.js'
+import Products from '../src/components/products/Products';
 
-class Routes extends Component {
-  render() {
-    return (
-      <Router history={hashHistory}>
+export default(
          <Route path="/" component={App}>
-	         <Route path="/home" component={Home}/>
-	         <Route path="/login" component={Login}/>
+           <IndexRoute component={Home}/>
+           <Route path="/login" component={Login}/>
+           <Route path="/products" component={Products}/>
+           <Route path="/products/:productName" component={Products}/>
          </Route>
-      </Router>
     )
-  }
-}
 
-export default Routes
