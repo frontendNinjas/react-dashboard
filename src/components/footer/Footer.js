0130c4736dcf47451f61ba12c6eba57b0cd1b30c
Harminder Kaur <harminder.kaur@rxpservices.com>
2017-04-05 10:15:26 +1000

    added header menu and avatar

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,8 +3,8 @@ import React from 'react';
 class Footer extends React.Component{
 	render(){
 		return (
-			<div>
-				This is the footer and copyright
+			<div className="footer">
+				FrontendNinjas Admin All Rights Reserved @2017
 			</div>
 		)
 	}
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,19 +6,44 @@ import IconButton from 'material-ui/IconButton';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import './headerStyle.css';
 
+import Avatar from 'material-ui/Avatar';
+import List from 'material-ui/List/List';
+import ListItem from 'material-ui/List/ListItem';
+
+import {
+  blue300,
+  indigo900,
+  orange200,
+  deepOrange300,
+  pink400,
+  purple500,
+} from 'material-ui/styles/colors';
+
+const style = {margin: -6};
+
+
 class Header extends React.Component {
   render() {
     return (
       <div className="headerContainer">
-        <IconMenu
-          iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
-          anchorOrigin={{horizontal: 'left', vertical: 'top'}}
-          targetOrigin={{horizontal: 'left', vertical: 'top'}}
-        >
-          <MenuItem linkButton containerElement={<Link to="/" />} primaryText="Home" />
-          <MenuItem linkButton containerElement={<Link to="/login" />} primaryText="Login" />
-          <MenuItem linkButton containerElement={<Link to="/products" />} primaryText="Products" />
-        </IconMenu>
+      <ul>
+         <li>
+            <List>
+              <ListItem disabled={true} leftAvatar={<Avatar src="../src/images/icon.jpg" size={30} style={style}/>}>
+              </ListItem>
+            </List>
+          </li>
+          <li>
+            <IconMenu
+              iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+              anchorOrigin={{horizontal: 'left', vertical: 'top'}}
+              targetOrigin={{horizontal: 'left', vertical: 'top'}}>
+              <MenuItem linkButton containerElement={<Link to="/" />} primaryText="Home" />
+              <MenuItem linkButton containerElement={<Link to="/login" />} primaryText="Login" />
+              <MenuItem linkButton containerElement={<Link to="/products" />} primaryText="Products" />
+            </IconMenu>
+          </li>
+        </ul>
       </div>
     );
   }
