f904e13cd2c1787d6c3afb1535020334fc838b61
Harminder Kaur <harminder.kaur@rxpservices.com>
2017-04-11 17:10:01 +1000

    theme page

--- src/admin/pages/themes.js
+++ src/admin/pages/themes.js
@@ -3,7 +3,11 @@ import React from 'react';
 
 class themes extends React.Component {
 	render(){
-		return <div> Look and Feel</div>
+		return <div> 
+		        <h3>Themes</h3>
+                <hr/>
+                  <img className="demo image" src="../src/images/themes.png" />
+		       </div>
 	}
 }
 
