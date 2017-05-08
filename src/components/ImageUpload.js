f1e3022b3c9681156e11243995af956f72aceaa5
Harminder Kaur <harminder.kaur@rxpservices.com>
2017-05-08 12:09:59 +1000

    image upload component

---
+++ src/components/ImageUpload.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const styles = {
+  imgPreview: {
+	  height: 298,
+	  width: 227
+  },
+  imgPreviewDiv:{
+    top:10,
+    position: 'relative',
+  },
+    uploadInput: {
+    cursor: 'pointer',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    width: '100%',
+    opacity: 0,
+  }
+};
+
+class ImageUpload extends React.Component{	
+
+ constructor(props) {
+    super(props);
+    this.state = {file: '',imagePreviewUrl: ''};
+  }
+
+    _handleSubmit(e) {
+    e.preventDefault();
+    // TODO: do something with -> this.state.file
+    console.log('handle uploading-', this.state.file);
+  }
+
+  _handleImageChange(e) {
+    e.preventDefault();
+
+    let reader = new FileReader();
+    let file = e.target.files[0];
+
+    reader.onloadend = () => {
+      this.setState({
+        file: file,
+        imagePreviewUrl: reader.result
+      });
+    }
+
+    reader.readAsDataURL(file)
+  }
+
+  render(){
+  	
+  	let {imagePreviewUrl} = this.state;
+    let $imagePreview = null;
+    if (imagePreviewUrl) {
+      $imagePreview = (<img style={styles.imgPreview} src={imagePreviewUrl} />);
+    } else {
+      $imagePreview = (<div>Please select an Image for Preview</div>);
+    }
+
+    return (
+      <div>
+        <form onSubmit={(e)=>this._handleSubmit(e)}>
+	    <RaisedButton
+	      label="Choose an Image"
+	      labelPosition="before"
+	      secondary={true}
+	      containerElement="label"
+	    >
+	      <input className="fileInput" style={styles.uploadInput} type="file" onChange={(e)=>this._handleImageChange(e)} />
+	    </RaisedButton>
+          <RaisedButton label="Upload" type="submit" onClick={(e)=>this._handleSubmit(e)} />
+        </form>
+        <div style={styles.imgPreviewDiv}>
+          {$imagePreview}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ImageUpload
--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import ImageUpload from '../../src/components/ImageUpload';
 
-const ProductForm = ({user,onSave,onChange,loading,errors}) => {
+const ProductForm = ({onSave,onChange}) => {
 	return (
 		<form>
 			<h1>Add Product</h1>
 			<TextField floatingLabelText="Title" /><br />
 			<TextField type="password" floatingLabelText="Price"  /><br />
-			<TextField floatingLabelText="Sku" /><br />
-			<TextField floatingLabelText="Description" /><br />
-			<TextField floatingLabelText="Type"  /><br />
-			<TextField floatingLabelText="Image"/><br />
-			<input type="submit" disabled={loading} value={loading ? 'Saving...': 'save'} onClick={onSave}/>
+			<TextField floatingLabelText="Description" /><br /><br />
+            <ImageUpload />
+			<br /><br />
+			<RaisedButton label="Submit" primary={true}/>
 		</form>
 	)
 }
