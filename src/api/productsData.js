aa692c5bb787e8405315cb2e4a0f90124b99a27c
Harminder Kaur <harminder.kaur@rxpservices.com>
2017-05-26 14:57:16 +1000

    added 3rd product into product data api

--- src/api/productsData.js
+++ src/api/productsData.js
@@ -11,6 +11,12 @@ var productsData =[
     bodyHtml: "React makes it painless to create interactive UIs, declarative, efficient, and flexible JavaScript library for building user interfaces. ... Your components tell React what you want to render – then React will efficiently update and render just the right components when your data changes. Here, ShoppingList is a React component class, or React component type.",
     price:"$20.89",
     image:"../../src/images/products/2.png"
+  },{
+            productid:"3",
+            productname: "Redux",
+            bodyHtml: "Redux it is a tool for managing both data-state and UI-state in JavaScript applications is a predictable state container for JavaScript apps. (If you're looking for a WordPress framework, check out Redux Framework.)",
+            price:"$22.19",
+            image:"../../src/images/products/3.png"
   }
 ];
 
